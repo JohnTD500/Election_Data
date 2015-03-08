01479ca4349edcc02cd4bca3d0ca0861f4841cef
Phil Hetzel <phetzel1@gmail.com>
2015-03-07 21:57:52 -0500

    added by registrant summary

--- Lobbying.R
+++ Lobbying.R
@@ -1,11 +1,18 @@
+#Prepare the environment for Lobbying data
 library(XML)
-library(plyr)
+library(dplyr)
 setwd("~/R Working Directory/Election_Data")
 rm(list=ls())
+
+#Place all items in this directory into a vector
 files <- dir('2015_1')
+
+#Create blank data frames to append to later
 filing <- data.frame()
 registrant <- data.frame()
 
+#For each file, we will read and parse the XML data. Then we will read in 
+#basic filing information, and registrant information.
 for (file in files){
     
     data <- xmlParse(paste('2015_1/',file,sep = ""), useInternalNodes = T)
@@ -23,7 +30,7 @@ for (file in files){
         xp <- "./Registrant"
         registrant <- xpathSApply(node, xp, xmlGetAttr, 'RegistrantID')
         registrant <- xpathSApply(node, xp, xmlGetAttr, 'RegistrantID')
-        data.frame(ID=id, RegistrantNumber=registrant, stringsAsFactors = FALSE)}))
+        data.frame(ID=id, RegistrantID=registrant, stringsAsFactors = FALSE)}))
     temp<- cbind(reg,data.frame(Year = year, Amount = amount, stringsAsFactors = F))
     filing <- rbind(filing, temp)
     
@@ -35,3 +42,12 @@ for (file in files){
     rtemp <- data.frame(RegistrantID = regNum, Name = regName, Address = regAd, stringsAsFactors = F)
     registrant <- unique(rbind(registrant, rtemp))
 }
+
+#Delete everything except data frames from memory
+rm(list=(ls()[ls()!=c("filing", "registrant")])) 
+
+by_registrant <- filing %>%
+    group_by(RegistrantID) %>%
+    summarise(Amount = sum(Amount,na.rm = T)) %>%
+    left_join(registrant) %>%
+    arrange(desc(Amount))
