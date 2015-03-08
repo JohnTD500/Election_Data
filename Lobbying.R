1e09288eac5c366a2cfe019d63709269837d7308
Phil Hetzel <phetzel1@gmail.com>
2015-03-08 13:57:41 -0400

    Issue compilation table

--- Lobbying.R
+++ Lobbying.R
@@ -7,11 +7,11 @@ setwd("~/R Working Directory/Election_Data")
 rm(list=ls())
 
 #Download the file directly from the government, then unzip into a directory
-#temp <- tempfile()
-#download.file('http://soprweb.senate.gov/downloads/2015_1.zip', temp, method = 'curl')
-#unz(temp, '2015_1')
-#unlink(temp)
-#rm(temp)
+temp <- tempfile()
+download.file('http://soprweb.senate.gov/downloads/2015_1.zip', temp, method = 'curl')
+unz(temp, '2015_1')
+unlink(temp)
+rm(temp)
 
 #Place all items in this directory into a vector
 files <- dir('2015_1')
@@ -23,12 +23,12 @@ client <- data.frame()
 issues <- data.frame()
 
 #For each file, we will read and parse the XML data. Then we will read in 
-#basic filing information, and registrant information.
+#basic filing, registrant, client, and issue information
 for (file in files){
     
     data <- xmlParse(paste('2015_1/',file,sep = ""), useInternalNodes = T)
     
-    #Filing Key data frame
+    #Filing Key data frame using the XPath language.
     filingID <- xpathSApply(data, "/PublicFilings/Filing", function(x) xmlGetAttr(x,'ID'))
     year <- xpathSApply(data, "/PublicFilings/Filing", function(x) xmlGetAttr(x,'Year'))
     received <- xpathSApply(data, "/PublicFilings/Filing", function(x) xmlGetAttr(x,'Received'))
@@ -36,8 +36,8 @@ for (file in files){
     reg <- xpathSApply(data, "/PublicFilings/Filing/Registrant", function(x) xmlGetAttr(x,'RegistrantID'))
     clientID <- xpathSApply(data, '/PublicFilings/Filing/Client', function(x) xmlGetAttr(x, 'ClientID'))
     
-    temp <- data.frame(FilingID = filingID, RegistrantID = reg,Year = year,
-                       Amount = amount, ClientID = clientID, stringsAsFactors = F)
+    temp <- data.frame(FilingID = filingID, RegistrantID = reg, Date = received, 
+                       Year = year, Amount = amount, ClientID = clientID, stringsAsFactors = F)
     filing <- rbind(filing, temp)
     
     #Client Key data frame
@@ -65,7 +65,7 @@ for (file in files){
     dups <- duplicated(registrant[,c('RegistrantID', 'Name')])
     registrant <- registrant[!dups,]
     
-    #Issues Key data frame
+    #Issues Key data frame. Issues are one to many with FilingID
     itemp <- do.call(rbind, xpathApply(data, "/PublicFilings/Filing", function(node) {
         id <- xmlGetAttr(node, "ID")
         xp <- ".//Issue"
@@ -77,19 +77,28 @@ for (file in files){
         }
         
         data.frame(FilingID=id, Type=issueCode, Description = issueDesc, stringsAsFactors = FALSE)}))
+    
     issues <- rbind(issues, itemp)
 }
+
+filing$Date <- as.Date(filing$Date, format = "%Y-%m-%d")
 
 #Delete everything except data frames from memory
 remove(reg, ctemp, rtemp, temp, amount, data, file, files, filingID, 
                received, regAd, regName, regNum, year, clientCountry, clientDesc,
        clientID, clientName, clientState, dups, cdups, regDesc, itemp)
 
+#Create data frame that shows the amount spent by each registrant
 by_registrant <- filing %>%
     group_by(RegistrantID) %>%
     summarise(Amount = sum(Amount,na.rm = T), Number = n()) %>%
     left_join(registrant) %>%
     arrange(desc(Amount))
 
+select_filing <- select(filing, FilingID, Amount)
 
-
+num_issues <- issues %>%
+    left_join(select_filing) %>%
+    group_by(Type) %>%
+    summarise(TotalNumber = n(), TotalDollars = sum(Amount, na.rm = T)) %>%
+    arrange(desc(TotalDollars))
